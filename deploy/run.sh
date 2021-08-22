6a15330cbdee4cfb0efc3c2bec83847764ecbd17
Nghiep <nghiep.p.phan@fundoo.me>
2021-08-22 13:58:29 +0700

    [develop] add deploy file

--- deploy/run.sh
+++ deploy/run.sh
@@ -3,7 +3,7 @@
 NETWORK_NAME=lms-network
 APP_NAME=lms-ui-docs
 
-docker run -d --name ${APP_NAME} \
+docker run -it -d --name ${APP_NAME} \
   --network lms-network \
   -e VIRTUAL_HOST="docs.learnhub.com.vn" \
   -e LETSENCRYPT_HOST="docs.learnhub.com.vn" \
